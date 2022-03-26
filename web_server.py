b39f3dc3d0712f7b84b4737519c7d14b02be7ada
renat <rinya1608@gmail.com>
2022-03-26 13:57:06 +0500

    MUS-16 add process for midi file from main service

--- web_server.py
+++ web_server.py
@@ -1,11 +1,23 @@
-from flask import Flask
+import re
+from flask import Flask, request, render_template, send_file
 from muse_handler import harmonize
+from music21 import note, stream, converter, chord
 app = Flask(__name__)
 
 
-@app.route("/&lt;username&gt;", methods=['GET'])
-def index(username):
-    return "Hello, %s!" % username
+@app.route("/api/file", methods=['POST'])
+def index():
+    file = request.files.get('file')
+    file.save('res/' + file.filename)
+
+    p = harmonize("tmp.mid")
+    p.write('midi','res.mid')
+
+
+    
+    #p.plot()
+    return send_file('res.mid')
+
 
 
 if __name__ == "__main__":
