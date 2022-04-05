9c256e569f3df40eb831159b03cad6cd489410c3
Vitaliy1234 <vitaliyf.108@gmail.com>
2022-04-06 00:29:17 +0500

    some changes in harmonizing

--- muse_handler.py
+++ muse_handler.py
@@ -1,4 +1,4 @@
-from music21 import note, stream, converter, chord
+from music21 import note, stream, converter, chord, midi
 from music21.analysis.discrete import DiscreteAnalysisException
 
 
@@ -12,43 +12,121 @@ HARMONY_RULES = [[1, 2, 3, 4, 5, 6],
 
 
 def harmonize(filename):
-    s = converter.parse(filename)
+    """
+    Function for harmonize melody
+    :param filename:
+    :return score: result score with accompany
+    """
+    score = converter.parse(filename)
 
-    ton = s.analyze('key')
-    tons_list = [ton]
-    tons_list.extend(ton.alternateInterpretations)
-    # print(tons_list)
+    tone = score.analyze('key')
+    tones_list = [tone]
+    tones_list.extend(tone.alternateInterpretations)
 
-    tonic = ton.getChord(type='whole').pitches[0]
-    dom = ton.getChord(type='whole').pitches[4]
-    subdom = ton.getChord(type='whole').pitches[3]
-    parall_tonic = ton.getChord(type='whole').pitches[5]
-    parall_dom = ton.getChord(type='whole').pitches[2]
-    parall_subdom = ton.getChord(type='whole').pitches[1]
-
-    for key in tons_list:
+    # iter over tones sorted by relevant
+    for key in tones_list:
         acc_p = stream.Part(id='accompany')
         measure_num = 1
         last_step = 0
-        last_chord = key
+
+        key_pitches = key.getChord().pitches
 
         print('key', key)
 
         continue_flag = False
-        # step = key.getChord().pitches.index(last_chord.getChord().pitches[0])
-        #
-        # if step not in HARMONY_RULES[last_step]:
-        #     continue
-
-        # measure_num += 1
-        exit_flag = False
         alternative_num = 0
-
+        exit_flag = False
+        next_measure = False
+        melody_chords = []
         while True:
+            measure_chords = []
+            used_chords = []
+            cur_measure = score.measure(measure_num).pitches
+            pitch_ind = 0
+
+            while pitch_ind < len(cur_measure):
+                if pitch_ind == len(used_chords):
+                    used_chords.append([])
+                if measure_num in [1, 8]:
+                    cur_measure = append_chord(key.getChord(), 'whole', measure_num)
+                    acc_p.append(cur_measure)
+                    melody_chords.append(0)
+                    measure_num += 1
+                    next_measure = True
+                    break
+                elif measure_num == 4:
+                    cur_measure = append_chord(key.getDominant(), 'whole', measure_num)
+                    acc_p.append(cur_measure)
+                    melody_chords.append(4)
+                    measure_num += 1
+                    next_measure = True
+                    break
+
+                if len(measure_chords) == 2 and measure_chords[1] != measure_chords[0]:
+                    pass
+                # if current note not in key notes, we return raw score
+                # TODO: fix handling notes beyond key
+                try:
+                    pitch_step = key_pitches.index(cur_measure[pitch_ind])
+                except Exception:
+                    return score
+
+                pitch_chords = [(pitch_step + i, chord.Chord([key_pitches[(pitch_step + i) % 7],
+                                                              get_third(key_pitches, pitch_step + i),
+                                                              get_fifth(key_pitches, pitch_step + i)],
+                                                             type='quarter')) for i in [0, 2, 4]]
+                pitch_chords_num = set([elem[0] for elem in pitch_chords])
+                print(measure_num, pitch_chords)
+                possible_chords = HARMONY_RULES[melody_chords[measure_num - 2]]
+                pitch_chords_num.intersection_update(possible_chords)
+                pitch_chords_num.difference_update(used_chords[pitch_ind])
+
+                print(pitch_chords_num)
+
+                if measure_num == 3:
+                    pitch_chords_num.discard(4)
+                if measure_num == 7:
+                    pitch_chords_num.discard(0)
+
+                if not pitch_chords_num:
+                    if pitch_ind == 0:
+                        return score
+                    pitch_ind -= 1
+                    continue
+
+                chord_to_add = pitch_chords_num.pop()
+
+                melody_chords.append(chord_to_add)
+                used_chords[pitch_ind].append(chord_to_add)
+                print(melody_chords, pitch_chords_num)
+                pitch_ind += 1
+                # exit_flag = True
+                # break
+
+            if next_measure:
+                try:
+                    score.measure(measure_num).analyze('key')
+                except Exception:
+                    print('jopa')
+                    break
+                next_measure = False
+                continue
+
+            measure_num += 1
+
+            try:
+                score.measure(measure_num).analyze('key')
+            except Exception:
+                break
+
+            if exit_flag:
+                break
+            continue
+
             if alternative_num > 5:
                 continue_flag = True
                 break
-            cur_ton = s.measure(measure_num).analyze('key')
+            cur_ton = score.measure(measure_num).analyze('key')
             chords = [cur_ton]
             chords.extend(cur_ton.alternateInterpretations)
             cur_chord = chords[alternative_num]
@@ -76,12 +154,10 @@ def harmonize(filename):
 
             cur_measure = stream.Measure(number=measure_num)
             cur_measure.append(chord.Chord([(cur_chord.getChord().root()), cur_chord.getChord().third, cur_chord.getChord().fifth], type='whole'))
-            # cur_measure.append(note.Note(cur_chord.getChord().third, type='whole'))
-            # cur_measure.append(note.Note(cur_chord.getChord().fifth, type='whole'))
             acc_p.append(cur_measure)
 
             try:
-                s.measure(measure_num + 1).analyze('key')
+                score.measure(measure_num + 1).analyze('key')
             except Exception:
                 break
 
@@ -92,12 +168,29 @@ def harmonize(filename):
         if continue_flag:
             continue
 
-        s.insert(0, acc_p)
+        score.insert(0, acc_p)
         break
 
-    return s
+    return score
+
+
+def append_chord(key, chord_type, measure_num):
+    cur_measure = stream.Measure(number=measure_num)
+    cur_measure.append(chord.Chord([(key.root()), key.third, key.fifth], type=chord_type))
+
+    return cur_measure
+
+
+def get_third(key_pitches, pitch_step):
+    return key_pitches[(pitch_step + 2) % 7]
+
+
+def get_fifth(key_pitches, pitch_step):
+    return key_pitches[(pitch_step + 4) % 7]
 
 
 if __name__ == '__main__':
     res = harmonize('data/test.mid')
-    res.show()
+    print(type(res))
+    res.plot()
+    # res.show()
